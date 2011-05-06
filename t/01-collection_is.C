dc5076f159c511107915e816fbf5595df05ca895
Eric David Moyer <eric_moyer@yahoo.com>
2011-05-05 20:06:25 -0400

    Added tests comparing vectors of strings with collection_is

--- t/01-collection_is.C
+++ t/01-collection_is.C
@@ -1,8 +1,10 @@
 #include <tap++/tap++.h>
+#include <vector>
+#include <string>
 
 int main(){
 	using namespace TAP;
-	plan(15);
+	plan(23);
 	//First, test the collection_is function
 	int col1[3] = {1,2,3};
 	int col2[3] = {1,2,3};
@@ -43,6 +45,34 @@ int main(){
 	lastResult = collection_is(col1,col1+3,col3,col3+3,"Should be todo");
 	TODO="";
 	not_ok(lastResult,"Expected 4 got 3 at index 2");
+
+	char const* col4[1]={"Morning"};
+	char const* col5[3]={"Morning","Afternoon","Evening"};
+	std::vector<std::string> v4(col4,col4+1), v5(col5,col5+3);
+	
+	TODO="Failing test to test the test harness";
+	lastResult = collection_is(v4.begin(),v4.end(),
+				   v5.begin(),v5.end(),"Should be todo");
+	TODO="";
+	not_ok(lastResult,"String vector got length 1, expected length 3");
+
+	TODO="Failing test to test the test harness";
+	lastResult = collection_is(v4.begin(),v4.end(),
+				   v5.begin(),v5.begin(),"Should be todo");
+	TODO="";
+	not_ok(lastResult,"String vector got length 1, expected length 0");
+
+	collection_is(v4.begin(),v4.end(),v5.begin(),v5.begin()+1,
+		      "Identical string vectors");
+
+	collection_is(v4.begin(),v4.begin(),v5.begin(),v5.begin(),
+		      "Identical empty string vectors");
+
+	TODO="Failing test to test the test harness";
+	lastResult = collection_is(v4.begin(),v4.end(),
+				   v5.begin()+1,v5.begin()+2,"Should be todo");
+	TODO="";
+	not_ok(lastResult,"String vectors same length, different contents");
 
 	return exit_status();
 }
